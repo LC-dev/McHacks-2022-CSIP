4edb00ac5b5702ad5df1abacb7ed5b4e645d75a1
paulhinta <paul_hinta@outlook.com>
2022-01-23 09:12:37 -0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ const theme = getTheme();
 
 function App() {
   useEffect(() => {
-    document.title = "Covid stringency index predictor";
+    document.title = "COVID-19 stringency index predictor";
   }, []);
 
   const [data, setData] = useState([]);
@@ -36,14 +36,12 @@ function App() {
   return (
     <div className="center">
       <div className="wrapper center">
-        <h1>Covid Stringency Index Predictor</h1>
+        <h1>COVID-19 Stringency Index Predictor</h1>
         <h2>Using AI to predict how strict public health measures should be</h2>
         <h4>
-          For your chosen country, select the values for the public health
-          performance indices below and get the corresponding policy
-          recommendation based on the predicted Stringency index! This is a
-          novel AI that we have trained ourselves to best equip you to navigate
-          the Covid-19 pandemic.
+          Using AI to recommend the level of public health restrictions, making
+          policymaking easier and more reliable. This is a novel AI that we have
+          trained ourselves to best equip you to navigate the COVID-19 pandemic.
         </h4>
       </div>
       <div
