7599186735e8d63f36a4e999712f42cfc64f41c8
paulhinta <paul_hinta@outlook.com>
2022-01-23 09:27:34 -0500

    fixed bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Deploy } from "./Component/Deploy/Deploy";
 import { InputForm } from "./Component/InputForm";
 
 import "./App.css";
@@ -15,7 +14,7 @@ function App() {
 
   const [data, setData] = useState([]);
   const getData = () => {
-    fetch("/output", {
+    fetch("http://localhost:5000/output", {
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
@@ -34,7 +33,18 @@ function App() {
     getData();
   }, []);
   return (
-    <div className="base center">
+    <div className="center">
+      <div className="wrapper center">
+        <h1>Covid Stringency Index Predictor</h1>
+        <h2>Using AI to predict how strict public health measures should be</h2>
+        <h4>
+          For your chosen country, select the values for the public health
+          performance indices below and get the corresponding policy
+          recommendation based on the predicted Stringency index! This is a
+          novel AI that we have trained ourselves to best equip you to navigate
+          the Covid-19 pandemic.
+        </h4>
+      </div>
       <div
         className="wrapper center"
         style={{ boxShadow: theme.effects.elevation4 }}
@@ -49,9 +59,9 @@ function App() {
           <p>Loading...</p>
         ) : (
           data.output.map((output, i) => (
-            <p className="center text-center" key={i}>
+            <h2 className="center text-center" key={i}>
               {output}
-            </p>
+            </h2>
           ))
         )}
       </div>
