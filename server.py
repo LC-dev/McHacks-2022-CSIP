9c53e88737414a1245c38601084728cc8d0f5a34
paulhinta <paul_hinta@outlook.com>
2022-01-23 09:29:19 -0500

    Final version (local)

--- server.py
+++ server.py
@@ -1,21 +1,19 @@
 from flask import Flask, jsonify, request
-from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 import pymongo
 from pymongo import MongoClient
 import certifi
 from bson.json_util import dumps, loads
+from bson import json_util
 import pickle
 from io import open
-import os
 import numpy as np
+import math
 
 filename="./finalized_model.sav"
 loaded_model = pickle.load(open(filename, 'rb'))
 
-p = int(os.environ.get("PORT", 5000))
-app = Flask(__name__, static_folder="client/build", static_url_path="")
-CORS(app)
+app = Flask(__name__)
 
 ca = certifi.where()
 
@@ -23,10 +21,25 @@ client=MongoClient("mongodb+srv://paulhinta:cP3&bG32@cluster0.xov9o.mongodb.net/
 #At the end we should change this to an environment variable so ppl don't my authentification
 db = client.Queries
 
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 #test
 query_params = "No live query"
 
 ## import AI Alg
+
+policy = {
+  0: "This country is well off with regards to Covid-19. As long as the government continues to educate the public on proper hygiene, no additional measures need to be taken.",
+  1: "This country is doing relatively well, but some measures should be taken. The government should prioritize supplying protective equipment such as masks to those who are most vulnerable, such as the elderly in retirement homes.",
+  2: "This country should be cautious. Government officials should have contingency plans set in place and be ready to act at any moment that a potential outbreak occurs.",
+  3: "This country should be cautious. A mask mandate should be implemented for all indoor activities. Officials should advise against nonessential in-person activities, such as certain school events and social gatherings.",
+  4: "This country should be cautious. A mask mandate should be implemented for all indoor activities. The government should mandate remote learning where possible, such as high schools and universities. Remote work is encouraged, where possible. Indoor social gatherings should be limited to 10 people.",
+  5: "This country has fallen behind in its COVID response plan. To reduce spread, a mask mandate should be implemented for all indoor activities. Where possible, schools should operate remotely. Remote work is encouraged, where possible. All social gatherings should be restricted to five people.",
+  6: "The public health system of this country is at a relatively high risk of collapsing. A mask mandate should be implemented. Proof of vaccination should be required for non-essential activities. Remote work should be mandated in hotspot regions. Where possible, schools should operate remotely.",
+  7: "The public health system of this country is at a high risk of collapsing. Mask and vaccine mandates should be implemented. Remote work should be mandated in hotspot regions. Schools should operate remotely. Social gatherings with people from different households should be banned.",
+  8: "The public health system of this country is failing. Wearing a mask and providing proof of vaccination should be required everywhere. All school should operate remotely. Non-essential (in-person) businesses should close. A fine should be introduced for those who violate social gathering rules. The government should consider closing certain international borders.",
+  9: "The public health system of this coutnry is failing. Mask and vaccine mandates should be required everywhere. All schools and non-essential businesses should operate remotely; those that can't should be closed, facing possible fines otherwise. International borders should close and domestic travel should be restricted."
+}
 
 populations = {
   "Belgium": 12000000,
@@ -90,7 +103,6 @@ def helloWorld():
 
   #sort through data
   for key in x:
-    print(x[key])
 
     if key=="_id":
       continue
@@ -100,10 +112,7 @@ def helloWorld():
       country = country.split(',')[0]
       country = country.split("'")[3]
       country = country.capitalize()
-    if x[key]=="title":
-      country = str(x[key]["text"])
-    if x[key]=="title":
-      country = str(x[key]["text"])
+      print(country)
       continue
     
     #double count
@@ -121,6 +130,7 @@ def helloWorld():
       continue
     else:
       try:
+        print(x[key], key)
         ret.append(float(x[key]))
       except TypeError:
         continue
@@ -133,8 +143,6 @@ def helloWorld():
     country = "Canada"
 
   user_input = np.asarray(ret)
-
-  print(user_input)
 
   #normalize data
   for i in range(4):
@@ -152,7 +160,11 @@ def helloWorld():
   #out = data out to FE, must be string!
   si = loaded_model.predict(final_input)[0]
 
-  out = [country, "Index: " + str(si)]
+  out = [country, "Index: " + str(round(si,2))]
+
+  policy_rec = policy[int(math.floor(si/10))]
+
+  out.append(policy_rec)
 
   return {"output": out}
 
@@ -161,7 +173,7 @@ def helloWorld():
 def input():
   x=request.get_json()
   res = db.Example.insert_one(x)
-  print(x)
   return query_params
 
-app.run(port=p, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run()
